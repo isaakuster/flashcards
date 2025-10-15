e5bdcc971b46fea7a1e2a75c4934431c18b294f3
isabela kuster <ricardotatarukrosa@gmail.com>
2025-10-15 00:29:17 +0000

    finalizado

---
+++ perguntas.js
@@ -0,0 +1,57 @@
+
+criaCartao(
+    'Programação',
+    'O que é uma tag?',
+    'A tag é um elemento da programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do japão?',
+    'A capital do japão é tóquio'
+)
+
+criaCartao(
+    'Filosofia',
+    'Diga o nume de um, filoso.',
+    'Sócrates.'
+)
+
+criaCartao(
+    'Historia',
+    'Quem descobriu o Brasil?',
+    'Pedro Alvares Cabral.'
+)
+
+criaCartao(
+    'Quimíca',
+    'Qual é o símbolo químico do oxigênio?',
+    'O símbolo é O.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual organela é responsavel pela produção de energia na célula?',
+    'Mitocôndria.'
+)
+
+
+criaCartao(
+    'Sociologia',
+    'O que é cultura?',
+    'Cultuta é um conjunto de valores, normas e comportamentos compartilhados por um grupo social.'
+)
+
+
+criaCartao(
+    'Robotica',
+    'O que é um sensor?',
+    'É um dispositivo que coleta informações do ambiente e envia para o robô, permitindo que ele reaja ou tome descisoẽs. '
+)
+
+criaCartao(
+    'Educação financeira',
+    'O que é o metodos 50,30,20',
+    'É uma forma simples de organizar o orçamento pessoal. '
+)
+
